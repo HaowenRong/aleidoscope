d6fe2cb245b7f1190675f86c73639d430b1ee4ee
HaowenRong <Alexrong1116@gmail.com>
2025-10-15 19:07:17 +0100

    Imported and used custom fonts with localFont

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import localFont from "next/font/local";
+
 // app/layout.tsx
 export const metadata = {
   title: 'phiew',
@@ -6,9 +8,20 @@ export const metadata = {
 
 import NaviBar from "@/components/NaviBar";
 
+const satoshi = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Regular.woff2", weight: '400', style: 'normal',
+      path: "../../public/fonts/Satoshi/Satoshi-Bold.woff2", weight: '700', style: 'normal'
+    },
+  ],
+  variable: "--font-satoshi"
+});
+
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={satoshi.variable}>
       <body>
         <NaviBar />
         {children}
