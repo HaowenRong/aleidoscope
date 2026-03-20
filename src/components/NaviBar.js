65812b2307129c9dea5fdafcfcd71b3feea35999
HaowenRong <Alexrong1116@gmail.com>
2026-03-20 10:56:59 +0000

    updated the hamburger menu to close on button click and when clicked outside

--- src/components/NaviBar.js
+++ src/components/NaviBar.js
@@ -1,35 +1,50 @@
 'use client'
 
 import Link from 'next/link'
-import { useState } from 'react'
 import '../styles/layout.css'
+
+import { useState, useEffect, useRef } from 'react'
 
 export default function NaviBar() {
   const [open, setOpen] = useState(false)
 
+  const naviRef = useRef(null)
+
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (naviRef.current && !naviRef.current.contains(e.target)) {
+        setOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => document.removeEventListener('mousedown', handleClickOutside)
+  }, [])
 
   return (
-    <nav className={`navi ${open ? 'open' : ''}`}>
+    <nav ref={naviRef} className={`navi ${open ? 'open' : ''}`}>
       <ul className='navi-list'>
         <li>
-          <Link href='/photo-stream' className='button'>
+          <Link href='/photo-stream' className='button' onClick={() => setOpen(false)}>
             <span className='label'>Highlights</span>
           </Link>
         </li>
         <li>
-          <Link href='/albums' className='button'>
+          <Link href='/albums' className='button' onClick={() => setOpen(false)}>
             <span className='label'>Albums</span>
           </Link>
         </li>
         <li>
-          <Link href='/about' className='button'>
+          <Link href='/about' className='button' onClick={() => setOpen(false)}>
             <span className='label'>About</span>
           </Link>
         </li>
       </ul>
 
       <button className='hamburg-icon' onClick={() => setOpen(!open)}>
-        <span>{open ? '✕' : '☰'}</span>
+        <span></span>
+        <span></span>
+        <span></span>
       </button>
     </nav>
   )
