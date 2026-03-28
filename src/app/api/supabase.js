ea3ed2144d5032910b2ea47a5d37a58f61dc9358
HaowenRong <Alexrong1116@gmail.com>
2026-03-28 11:55:24 +0000

    implemented api functionality for accessing supabase

---
+++ src/app/api/supabase.js
@@ -0,0 +1,79 @@
+import { createClient } from "@supabase/supabase-js"
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY
+)
+
+// get album data
+export async function getAllAlbumData() {
+  const { data, error } = await supabase
+    .from('albums')
+    .select('*')
+
+  if (error) {
+    console.error(error)
+    return []
+  }
+  
+  return data
+}
+
+// get single album data
+export async function getAlbumData(name) {
+  const { data, error } = await supabase
+    .from('albums')
+    .select('*')
+    .eq('album_name', name)
+    .single()
+
+  if (error) {
+    console.error(error)
+    return null
+  }
+
+  return data
+}
+
+// get single image
+export function getImageUrl(path) {
+  const { data } = supabase.storage
+    .from('albums')
+    .getPublicUrl(path)
+
+  return data.publicUrl
+}
+
+// get all images from a folder
+export async function getFolder(folder) {
+  const { data, error } = await supabase.storage
+    .from('albums')
+    .list(folder, {
+      sortBy: { column: 'created_at', order: 'asc' }
+    })
+
+  if (error) {
+    console.error(error)
+    return []
+  }
+
+  return data.map(file => getImageUrl(`${folder}/${file.name}`))
+}
+
+// get the number of photos in a folder
+export async function getNumInFolder(folder) {
+  const { data, error } = await supabase.storage
+    .from('albums')
+    .list(folder, {
+      sortBy: { column: 'created_at', order: 'asc' }
+    })
+
+  if (error) {
+    console.error(error)
+    return 0
+  }
+
+  return data.length
+}
+
+export default supabase
