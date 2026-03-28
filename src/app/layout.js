715a0a06703c2e2a8a60eea04a7e01724fb19bbd
HaowenRong <Alexrong1116@gmail.com>
2026-03-28 11:18:04 +0000

    removed unused fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,14 +8,6 @@ export const metadata = {
 
 const manrope = localFont({
   src: [
-    { path: '../../public/fonts/manrope/Manrope-ExtraLight.woff2', weight: '200', style: 'normal' },
-    { path: '../../public/fonts/manrope/Manrope-Light.woff2',      weight: '300', style: 'normal' },
-    { path: '../../public/fonts/manrope/Manrope-Regular.woff2',    weight: '400', style: 'normal' },
-    { path: '../../public/fonts/manrope/Manrope-Medium.woff2',     weight: '500', style: 'normal' },
-    { path: '../../public/fonts/manrope/Manrope-SemiBold.woff2',   weight: '600', style: 'normal' },
-    { path: '../../public/fonts/manrope/Manrope-Bold.woff2',       weight: '700', style: 'normal' },
-    { path: '../../public/fonts/manrope/Manrope-ExtraBold.woff2',  weight: '800', style: 'normal' },
-    { path: '../../public/fonts/manrope/Manrope-ExtraBold.woff2',  weight: '800', style: 'normal' },
     { path: '../../public/fonts/manrope/Manrope-Variable.woff2',   weight: '100 900', style: 'normal' }
   ],
   variable: '--font-manrope',
